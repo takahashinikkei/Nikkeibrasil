02d482e343478a88d134c2aa069e8b0564803a75
takahashinikkei <dehtakahashi@gmail.com>
2026-09-24 20:16:55 -0300

    Refresh app cache for logo update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nikkeibrasil-v2';
+const CACHE='nikkeibrasil-v3';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-nikkeibrasil.svg','./nikkei-logo.svg'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
