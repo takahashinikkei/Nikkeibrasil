c5ddc2de3511ad0966f0e8539a7b9317b9207b2f
takahashinikkei <dehtakahashi@gmail.com>
2026-09-24 18:21:06 -0300

    Inicializa Nikkei Brasil: sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='nikkeibrasil-v1';
+const APP_SHELL=['./','./index.html','./manifest.json','./icon-nikkeibrasil.svg','./icon-nikkeibrasil.svg'];
+self.addEventListener('install',event=>{
+ event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+ event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET')return;
+ const isNavigation=event.request.mode==='navigate';
+ event.respondWith(
+  fetch(event.request,{cache:isNavigation?'no-store':'default'}).then(response=>{
+   const copy=response.clone();
+   caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+   return response;
+  }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))
+ );
+});
