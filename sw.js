b4334d7d0e417c23d9f581b6d752073ce6ae8270
takahashinikkei <dehtakahashi@gmail.com>
2026-09-25 00:20:45 -0300

    Atualizar cache para versão 18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nikkeibrasil-v17';
+const CACHE='nikkeibrasil-v18';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-nikkeibrasil.svg','./nikkei-logo.svg','./nikkei-logo.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
