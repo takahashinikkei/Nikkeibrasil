404301b5b4f96352ef5527bc35169d9da773c5c0
takahashinikkei <dehtakahashi@gmail.com>
2026-09-24 23:43:46 -0300

    Atualizar cache para versão 13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nikkeibrasil-v12';
+const CACHE='nikkeibrasil-v13';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-nikkeibrasil.svg','./nikkei-logo.svg','./nikkei-logo.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
