051429900aef966b5fd9f5d1a7939a9bd6b1c1ff
takahashinikkei <dehtakahashi@gmail.com>
2026-09-25 00:36:34 -0300

    Atualizar cache para versao 20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nikkeibrasil-v19';
+const CACHE='nikkeibrasil-v20';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-nikkeibrasil.svg','./nikkei-logo.svg','./nikkei-logo.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
