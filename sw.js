4e70e232babf71c083f95c933104d67611a5e343
takahashinikkei <dehtakahashi@gmail.com>
2026-09-24 22:43:59 -0300

    Refresh app after temporary login removal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nikkeibrasil-v4';
+const CACHE='nikkeibrasil-v5';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-nikkeibrasil.svg','./nikkei-logo.svg','./nikkei-logo.png'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
