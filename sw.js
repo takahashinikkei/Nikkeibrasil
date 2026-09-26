99d82e849464c018dd71971e0cc1cc6880512517
takahashinikkei <dehtakahashi@gmail.com>
2026-09-26 09:29:56 -0300

    Atualizar cache de placa e selecao do motorista

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nikkeibrasil-v40';
+const CACHE='nikkeibrasil-v41';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-nikkeibrasil.svg','./nikkei-logo.svg','./nikkei-logo.png','./nikkei-logo-wide.jpg'];
 self.addEventListener('install',event=>{
  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
